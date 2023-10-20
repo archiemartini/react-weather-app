bffa591cc9d09d23b4d6199d7fbdde4f15f6df29
Archie Martin <archie.cgm@gmail.com>
2023-10-20 13:50:54 +0100

    added search and api

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b9052fa1b0mshe5a3cada7e50498p1a517cjsn6c055d673a00',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+
+// openweather
+
+// c47a0fc433237a8db146e0e1f2b6f46b
